45b764aa038ef4635d6ae3d5d71acfc079407193
midhun32 <midhunmidhun568@gmail.com>
2024-09-10 19:21:39 +0530

    Responsive was correctly set

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,39 @@ export default function Home() {
   const typingSpeed = 150;
   const deletingSpeed = 100;
   const pauseBetween = 1000;
-  const dataText = ["MIDHUN M", "Web Developer"];
+  const dataText = ["MIDHUN M", "Web Developer", "Data Scientist"];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // for scroll function useeffect
+  const slides = [
+    {
+      title: 'Certificate 1',
+      image: './ds.png',
+    },
+    {
+      title: 'Certificate 2',
+      image: './reactcef.png',
+    },
+    {
+      title: 'Certificate 3',
+      image: './Salesforce.png',
+    },
+  ];
+
+  const openModal = (index) => {
+    setCurrentIndex(index);
+    setIsModalOpen(true);
+  };
+  const closeModal = () => setIsModalOpen(false);
+
+  const goToPrevious = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
+  };
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+  };
+
+  // for scroll function
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 0) {
@@ -87,7 +117,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40 pt-20">
-        <section id='home' className="min-h-screen">
+        <section id='home' className="min-h-screen py-8 sm:py-10 lg:py-12">
           <nav
             className={`px-32 py-6 mb-8 flex justify-between dark:text-white fixed top-0 left-0 w-full z-50 bg-white dark:bg-gray-900 items-center transition-all duration-300 ${isScrolled ? "bg-white/30 backdrop-filter backdrop-blur-lg border-b border-gray-200 dark:border-gray-800" : ""
               }`}
@@ -155,7 +185,7 @@ export default function Home() {
               </ul>
             </div>
           )}
-          <div className="relative mx-auto w-78 h-78 md:w-56 md:h-56 mt-20">
+          <div className="relative mx-auto w-32 h-32 md:w-56 md:h-56 mt-20"> {/* Adjusted size */}
             <div className="absolute inset-0 bg-teal-500 rounded-full animate-jelly"></div>
             <div className="relative w-full h-full rounded-full overflow-hidden bg-white shadow-xl hover:scale-105 transition-transform">
               <Image
@@ -182,29 +212,29 @@ export default function Home() {
               "Hello and welcome! I'm Midhun, a passionate software developer who loves creating innovative solutions. Explore my portfolio to see my work and learn about my journey in tech."
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <a href="https://github.com/Midhun62" target="_blank" rel="noopener noreferrer" className="hover:text-[#181717] dark:hover:text-[#181717] transition-colors">
+              <a href="https://github.com/Midhun62" target="_blank" rel="noopener noreferrer" className="hover:text-[#181717] dark:hover:text-[#181717] transition-colors shadow-md">
                 <AiFillGithub />
               </a>
-              <a href="https://www.linkedin.com/in/midhun-m-851b86225/" target="_blank" rel="noopener noreferrer" className="hover:text-[#0077B5] dark:hover:text-[#0077B5] transition-colors">
+              <a href="https://www.linkedin.com/in/midhun-m-851b86225/" target="_blank" rel="noopener noreferrer" className="hover:text-[#0077B5] dark:hover:text-[#0077B5] transition-colors shadow-lg">
                 <AiFillLinkedin />
               </a>
-              <a href="http://www.youtube.com/@TechMasterAITut" target="_blank" rel="noopener noreferrer" className="hover:text-[#FF0000] dark:hover:text-[#FF0000] transition-colors">
+              <a href="http://www.youtube.com/@TechMasterAITut" target="_blank" rel="noopener noreferrer" className="hover:text-[#FF0000] dark:hover:text-[#FF0000] transition-colors shadow-lg">
                 <AiFillYoutube />
               </a>
             </div>
           </div>
         </section>
-        <section id="about" className="py-10 mb-16">
+        <section id="about" className="py-8">
           <div className="max-w-7xl mx-auto px-4">
             <h3 className="text-3xl py-1 dark:text-white pb-8">About Me</h3>
-            <div className="text-xl dark:text-white">I am a frontend web developer with a passion for creating interactive and responsive web applications. I have experience working with HTML, CSS, JavaScript, React, Mysql and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and I am excited to work with others to create amazing applications.
+            <div className="text-xl dark:text-white">I am a frontend web developer with a passion for creating interactive and responsive web applications. I have experience working with HTML, CSS, JavaScript, React, Python, Mysql and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and I am excited to work with others to create amazing applications.
               {/* techniology cards */}
               <div className="mt-12 mb-12">
                 <h2 className="text-3xl font-bold text-center mb-8 text-teal-500 dark:text-teal-500">Tech-Skills</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-12 mb-12">
                   {/* card 1 */}
                   <div className="relative bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl hover:scale-105 transition-transform duration-300">
-                    <div className="p-6">
+                    <div className="p-6 hover:bg-gradient-to-r from-cyan-300 to-teal-300">
                       <div className="flex items-center justify-center h-16 w-16 rounded-full mb-4 mx-auto">
                         <img src="/html5.png" alt="HTML Icon" className="h-16 w-16" />
                       </div>
@@ -214,7 +244,7 @@ export default function Home() {
 
                   {/* card 2 */}
                   <div className="relative bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl hover:scale-105 transition-transform duration-300">
-                    <div className="p-6">
+                    <div className="p-6 hover:bg-gradient-to-r from-cyan-300 to-teal-300">
                       <div className="flex items-center justify-center h-16 w-16 rounded-full mb-4 mx-auto">
                         <img src="/css.png" alt="CSS Icon" className="h-16 w-16" />
                       </div>
@@ -224,7 +254,7 @@ export default function Home() {
 
                   {/* card 3 */}
                   <div className="relative bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl hover:scale-105 transition-transform duration-300">
-                    <div className="p-6">
+                    <div className="p-6 hover:bg-gradient-to-r from-cyan-300 to-teal-300">
                       <div className="flex items-center justify-center h-16 w-16 rounded-full mb-4 mx-auto">
                         <img src="js.png" alt="JavaScript Icon" className="h-16 w-16" />
                       </div>
@@ -234,7 +264,7 @@ export default function Home() {
 
                   {/* card 4 */}
                   <div className="relative bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl hover:scale-105 transition-transform duration-300">
-                    <div className="p-6">
+                    <div className="p-6 hover:bg-gradient-to-r from-cyan-300 to-teal-300">
                       <div className="flex items-center justify-center h-16 w-16 rounded-full mb-4 mx-auto">
                         <img src="/react.svg" alt="JavaScript Icon" className="h-16 w-16" />
                       </div>
@@ -244,7 +274,7 @@ export default function Home() {
 
                   {/* card 5 */}
                   <div className="relative bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl hover:scale-105 transition-transform duration-300">
-                    <div className="p-6">
+                    <div className="p-6 hover:bg-gradient-to-r from-cyan-300 to-teal-300">
                       <div className="flex items-center justify-center h-16 w-16 rounded-full mb-4 mx-auto">
                         <img src="/python.svg" alt="JavaScript Icon" className="h-14 w-14" />
                       </div>
@@ -254,7 +284,7 @@ export default function Home() {
 
                   {/* card 6 */}
                   <div className="relative bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl hover:scale-105 transition-transform duration-300">
-                    <div className="p-6">
+                    <div className="p-6 hover:bg-gradient-to-r from-cyan-300 to-teal-300">
                       <div className="flex items-center justify-center h-16 w-16 rounded-full mb-4 mx-auto">
                         <img src="/mysql.svg" alt="JavaScript Icon" className="h-16 w-16" />
                       </div>
@@ -264,7 +294,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="mt-16 mb-12">
+            <div className="mt-16 mb-20">
               <h2 className="text-3xl font-bold text-center mb-8 text-teal-500 dark:text-teal-500">Education</h2>
               <div className="w-full flex justify-center items-center text-blue-50 pt-10">
                 <div className="max-w-7xl mx-auto w-full grid grid-cols-9">
@@ -337,14 +367,105 @@ export default function Home() {
                       <p className="sm:text-sm text-xs">Currently looking for an organization where I can leverage my skills in JavaScript, React, Next.js, and more to contribute to exciting projects and continue growing my expertise. Open to roles that offer learning and development opportunities.</p>
                     </div>
                   </div> */}
+
                 </div>
               </div>
+            </div>
+            <div>
+              <h2 className="text-3xl font-bold text-center mb-14 text-teal-500 dark:text-teal-500">Certificates</h2>
+              <div className="relative w-full max-w-3xl mx-auto">
+                {/* Arrow Buttons and Certificate Image */}
+                <div className="relative flex items-center justify-between">
+                  {/* Left Arrow Button */}
+                  <button
+                    className="p-2 md:p-4 bg-gray-200 text-gray-800 rounded-full shadow-md hover:bg-gray-300 transform -translate-y-1/2 sm:-translate-y-0 mr-4"
+                    onClick={goToPrevious}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2}
+                      stroke="currentColor"
+                      className="w-5 h-5 md:w-6 md:h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 19l-7-7 7-7"
+                      />
+                    </svg>
+                  </button>
+
+                  {/* Certificate Image */}
+                  <div className="relative w-full">
+                    <img
+                      src={slides[currentIndex].image}
+                      alt={slides[currentIndex].title}
+                      className="w-full h-auto object-cover cursor-pointer rounded-lg"
+                      onClick={() => openModal(currentIndex)} // Opens modal on image click
+                    />
+                  </div>
+
+                  {/* Right Arrow Button */}
+                  <button
+                    className="p-2 md:p-4 bg-gray-200 text-gray-800 rounded-full shadow-md hover:bg-gray-300 transform -translate-y-1/2 sm:-translate-y-0 ml-4"
+                    onClick={goToNext}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2}
+                      stroke="currentColor"
+                      className="w-5 h-5 md:w-6 md:h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              {/* Modal for Enlarged Image */}
+              {isModalOpen && (
+                <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
+                  <div className="relative max-w-4xl mx-auto">
+                    <img
+                      src={slides[currentIndex].image}
+                      alt={slides[currentIndex].title}
+                      className="w-full h-auto object-contain rounded-lg"
+                    />
+                    <button
+                      className="absolute top-4 right-4 text-white bg-gray-800 p-2 rounded-full"
+                      onClick={closeModal} // Closes modal on click
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </section>
 
         <section id="Projects">
-          <div>
+          <div className="py-8">
             <h3 className="text-2xl md:text-3xl py-1 dark:text-white">Projects</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               As I continue to grow in my journey as a web developer, I am currently working on a variety of web-based projects, refining my skills and expanding my portfolio. I am actively learning and preparing to venture into app development, with plans to start building innovative applications very soon.
@@ -409,7 +530,7 @@ export default function Home() {
 
               <form
                 className="w-full bg-white p-8 rounded-lg shadow-lg dark:bg-gray-800"
-                action="/api/contact"
+                action="https://formspree.io/f/meojdybq"
                 method="POST"
               >
                 <div className="mb-4">
